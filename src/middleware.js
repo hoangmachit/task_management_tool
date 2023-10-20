718547253174b5076b8fc1d0a4d89afb06756fb5
AGL_Mạch Văn Hoàng (ホアン) <81142674+machvanhoang@users.noreply.github.com>
2023-10-20 11:29:22 +0700

    Middleware

---
+++ src/middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+export async function middleware(request) {
+    const { pathname } = request.nextUrl;
+    console.log(`>>>>Pathname: ${pathname}`);
+    return NextResponse.next();
+}
+export const config = {
+    matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico|assets).*)'
+    ],
+}
